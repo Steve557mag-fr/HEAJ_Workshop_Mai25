952d3c77c94d9c5c66de0826b2f0fa869ee0cd04
Steve557mag-fr <cbuchelotsteve557@gmail.com>
2025-03-28 23:20:39 +0100

    Add card manager

--- Assets/Resources/Scripts/Receipts/ReceiptObject.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ReceiptObject", menuName = "Scriptable Objects/ReceiptObject")]
-public class ReceiptObject : ScriptableObject
-{
-
-    [SerializeField] CardBase card;
-    
-}
