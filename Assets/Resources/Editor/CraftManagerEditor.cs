d965fd2625b4c096c3b41b7a445182f64345c84e
COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
2025-04-27 09:59:50 +0200

    Polish craft manager editor

--- Assets/Resources/Editor/CraftManagerEditor.cs
+++ Assets/Resources/Editor/CraftManagerEditor.cs
@@ -10,6 +10,7 @@ public class CraftManagerEditor : EditorWindow
 
     GameObject pattern;
     GameObject resultCraft;
+    List<CraftDataObject> crafts;
 
     int currentCraftIdx = 0;
 
@@ -21,7 +22,7 @@ public class CraftManagerEditor : EditorWindow
 
     void AddNewCraft() {
         string path = EditorUtility.SaveFilePanel(
-            "where ?", "Assets/Resources/Data/Crafts", "NewCraft", "assets"
+            "where ?", "Assets/Resources/Data/Crafts", "NewCraft", "asset"
         );
         if (path == "") return;
 
@@ -30,28 +31,37 @@ public class CraftManagerEditor : EditorWindow
         CraftDataObject newCraft = ScriptableObject.CreateInstance<CraftDataObject>();
         AssetDatabase.CreateAsset( newCraft, path );
         AssetDatabase.SaveAssets();
-        
+
     }
     void RemCurrentCraft() {
-    
+        CraftDataObject currentCraftData = crafts[currentCraftIdx];
+        if (currentCraftData == null) return;
+
+        AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(currentCraftData));
+        AssetDatabase.SaveAssets();
+
     }
 
     private void OnGUI()
     {
         //Get the current craft data
         CraftDataObject currentCraftData = null;
-        List<CraftDataObject> crafts = AssetDatabase.FindAssets("t:CraftDataObject")
+        crafts = AssetDatabase.FindAssets("t:CraftDataObject")
             .Select(guid => (CraftDataObject) AssetDatabase.LoadAssetAtPath( 
                 AssetDatabase.GUIDToAssetPath(guid)    
             , typeof(CraftDataObject))).ToList();
 
         //Top bar of the tool
+        EditorGUILayout.Space(15);
         EditorGUILayout.BeginHorizontal();
-        EditorGUILayout.Popup(0, crafts.Select(o => o.name).ToArray());
+        currentCraftIdx = EditorGUILayout.Popup(currentCraftIdx, crafts.Select(o => o.name).ToArray());
         if(GUILayout.Button("+")) AddNewCraft();
         if(GUILayout.Button("-")) RemCurrentCraft();
         EditorGUILayout.EndHorizontal();
+        EditorUtils.GuiLine(padding: 15);
 
+        if (currentCraftIdx >= crafts.Count || currentCraftIdx < 0) return;
+        currentCraftData = crafts[currentCraftIdx];
 
         //Display of the craft
         pattern = (GameObject) EditorGUILayout.ObjectField("craft pattern: ", pattern, typeof(GameObject), false);
