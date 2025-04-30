5fbb329f5ac7abde775de6fffc1bb4570e7a8cf0
COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
2025-04-30 15:31:27 +0200

    Create GItemObject + PlayerState

---
+++ Assets/Resources/Scripts/Gameplay/PlayerState.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    [SerializeField] Dictionary<GameItemObject, int> inventory;
+
+    public void ModifyQuantity(GameItemObject item, int quantity = 1, bool markInfinite = false)
+    {
+        if (inventory.ContainsKey(item))
+        {
+            int prevQ = inventory[item];
+            inventory.Add(item, markInfinite ? -1 : prevQ + quantity);
+        }
+        else inventory.Add(item, markInfinite ? -1 : quantity);
+    }
+
+    
+
+}
