ff370743322b678c61a36777850a0b3833893a81
COL1 C'EST MOI <57326899+Steve557mag-fr@users.noreply.github.com>
2025-05-13 10:07:34 +0200

    encooorreeee

--- Assets/Resources/Scripts/Gameplay/Player.cs
+++ Assets/Resources/Scripts/Gameplay/Player.cs
@@ -4,15 +4,7 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
 
-<<<<<<< Updated upstream
     bool inNavigation = false;
-=======
-    [SerializeField] GameObject uiNavigationContainer;
-
-    public void ToggleUINavigation()
-    {
-        uiNavigationContainer.SetActive(uiNavigationContainer.activeSelf);
->>>>>>> Stashed changes
 
     public void ToggleUINavigation()
     {
